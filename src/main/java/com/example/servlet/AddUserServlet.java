07bbc298518d91a29c8996c04d318abd3cbb8d16
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2024-02-09 19:15:32 +0300

    Finish : Module #2. Task #1. Servlets

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,49 @@
 package com.example.servlet;
 
-public class AddUserServlet {
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
+
+@WebServlet("/add")
+
+public class AddUserServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        User user = new User(firstName,lastName);
+
+        Warehouse warehouse = Warehouse.getInstance();
+        warehouse.addUser(user);
+
+        req.setAttribute("user", user);
+        req.setAttribute("userName",firstName);
+        req.setAttribute("lastName",lastName);
+        try {
+            resp.sendRedirect("/add");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            RequestDispatcher requestDispatcher=req.getRequestDispatcher("jsp/add.jsp");
+            requestDispatcher.forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     //write your code here!
 }
